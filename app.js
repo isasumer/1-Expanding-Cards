6386f570ee305998db613c5afaa16be75ceaadb4
isa <sumerisa5@gmail.com>
2021-07-28 14:33:37 +0300

    projects finished

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-//Selecting all sections with class section
-const sections = document.querySelectorAll(".section")
 
-//On click event for each section
+// Selecting all sections with class of section
+const sections = document.querySelectorAll('.section')
 
-sections.forEach((section) =>{
-    section.addEventListener("click", () =>{
-        //remove active class from all another section
-        //and add it to the selected section
-        sections.forEach((section) =>{
-            section.classList.remove("active")
-        })
-        section.classList.add("active")
+// On click event for each section 
+sections.forEach((section)=>{
+    section.addEventListener('click',()=>{
+
+        sections.forEach((section) => {
+            section.classList.remove('active')
     })
+    section.classList.add('active')
+})
 })
